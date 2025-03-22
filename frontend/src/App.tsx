e9fc696bf6d116e4495c1081c687515061366fb0
AditHash <168162359+AdityaWorkmates@users.noreply.github.com>
2025-03-23 02:14:26 +0530

    Initial commit: Set up QR Code Generator project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,17 @@ function App() {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/generate-qr', {
+      // for local uncomment below code
+      // const response = await fetch('http://127.0.0.1:8000/generate-qr', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({ text }),
+      // });
+
+      // for deployment in docker uncomment below code
+      const response = await fetch('http://backend:8000/generate-qr', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
